09e35f92807cf3ab946dafa455bcb86f9fac6f36
Tyrell Teppang <tyrellmarcus.teppang@gcash.com>
2025-08-07 17:06:43 +0800

    Exercises 1 - 3 Done

--- JC-Exploring-JavaDateTime/src/main/java/org/example/Main.java
+++ JC-Exploring-JavaDateTime/src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
 
+        //Exercise 3: LocalDateTime and DateTimeFormatter
+        LocalDateTime event = LocalDateTime.of(2025, 11, 27, 19, 0, 0);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMM dd, yyyy 'at' hh:mm a");
+
+        System.out.println("Default format: "+ event);
+        System.out.println("Custom format: "+ event.format(dtf));
+
+        //Prediction: Default format: 2025-11-27 19:0:0 \n Custom format: NOV 27, 2025 at 7:00 PM
+        //Actual:
     }
 }
