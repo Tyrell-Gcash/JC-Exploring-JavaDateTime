d355236ef94a4964fbf5adb70072a1494a41c7b6
Tyrell Teppang <tyrellmarcus.teppang@gcash.com>
2025-08-07 17:25:03 +0800

    Lab Complete with readme

--- JC-Exploring-JavaDateTime/src/main/java/org/example/Main.java
+++ JC-Exploring-JavaDateTime/src/main/java/org/example/Main.java
@@ -1,20 +1,23 @@
 package org.example;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Main {
     public static void main(String[] args) {
 
-        //Exercise 3: LocalDateTime and DateTimeFormatter
-        LocalDateTime event = LocalDateTime.of(2025, 11, 27, 19, 0, 0);
+        //Exercise 6: Period - Measuring a Span of Time
+        LocalDate date1 = LocalDate.of(2024, 3, 15);
+        LocalDate date2 = LocalDate.of(2026, 7, 20);
 
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMM dd, yyyy 'at' hh:mm a");
+        Period period = Period.between(date1, date2);
 
-        System.out.println("Default format: "+ event);
-        System.out.println("Custom format: "+ event.format(dtf));
+        System.out.print("The period between the two dates is: ");
+        System.out.print(period.getYears() + " years, ");
+        System.out.print(period.getMonths() + " months, and ");
+        System.out.println(period.getDays() + " days.");
 
-        //Prediction: Default format: 2025-11-27 19:0:0 \n Custom format: NOV 27, 2025 at 7:00 PM
+        //Prediction: The period between the two dates is: 2 years, 4 months, and 5 days. \n
         //Actual:
     }
 }
